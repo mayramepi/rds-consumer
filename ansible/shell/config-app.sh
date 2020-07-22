116bb156690855be29c09b60564bcb0ca072e5e8
Anibal Barbosa <abarbosa@buenosaires.gob.ar>
2020-07-22 16:58:26 -0300

    ansible config

--- ansible/shell/config-app.sh
+++ ansible/shell/config-app.sh
@@ -9,8 +9,7 @@ if [ -z $1 ]; then
 else
   APP_PATH=$1
 fi
-
-DOMINIO_FRONT=$2
+DATABASE_URL=$2
 URL_BACK=$3
 HTTPD_CONFIG=$4
 HTTPD_DAEMON=$5
@@ -19,47 +18,11 @@ DOMINIO_FRONT=$3
 URL_BACK=$4
 HTTPD_CONFIG=$4
 HTTPD_DAEMON=$5
-
-
-"
-
 cd $APP_PATH/source
 
-########################################################################################################################
+source config.sh
+startjava
 
-########################################################################################################################
-
-
-
-
-########################################################################################################################
-
-
-
-########################################################################################################################
-
-
-
-
-########################################################################################################################
-
-
-# CONFIG_HTTPD="//opt/rh/httpd24/root/etc/httpd/conf.d/front-eval.conf"
-cp ../ansible/inventory/apache.conf.dist $HTTPD_CONFIG
-sed -i "s/PROJECT_ROOT/${APP_PATH//\//\\/}/g" $HTTPD_CONFIG
-sed -i "s/DOMINIO_FRONT/${DOMINIO_FRONT}/g" $HTTPD_CONFIG
-
-########################################################################################################################
-##################CONFIGURANDO EL FRONT ##########################################################################################
-mkdir front/assets/config
-cp ../ansible/inventory/config.json.dist front/assets/config/config.json
-sed -i "s/URL_BACK/${URL_BACK//\//\\/}/g" front/assets/config/config.json
-
-#service httpd restart
-#service httpd24-httpd restart
-#service httpd24-httpd status -l
-service $HTTPD_DAEMON restart
-service $HTTPD_DAEMON status -l
 
 ########################################################################################################################
 
