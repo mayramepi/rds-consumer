e481bebd95794d14802c5856960373e8a993a2ff
Anibal Barbosa <abarbosa@buenosaires.gob.ar>
2020-07-22 16:55:00 -0300

    ansible config

--- ansible/updatedev.sh
+++ ansible/updatedev.sh
@@ -3,8 +3,8 @@
 ansible-playbook -i ./inventory/hosts update.yml \
             -u root \
             -e env=dev \
-            -e repository="git@git-asi.buenosaires.gob.ar:usuarioQA/asi-344-eval_desem_front.git" \
-            -e folder="asi-344-eval_desem_front" \
-            -e branch="development" \
-            -e path="/var/www/html/disco50/java" \
+            -e repository="git@ssh-repositorio-ce-asi.buenosaires.gob.ar:reingenieria-recibos-sueldo/rds-consumer.git" \
+            -e folder="rds-consumer" \
+            -e branch="dev" \
+            -e path="/java/rds-consumer" \
             -vv
