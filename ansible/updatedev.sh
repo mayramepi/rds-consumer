b3177ff57963f3e9995cb9c07031e0d1f66518a5
Anibal Barbosa <abarbosa@buenosaires.gob.ar>
2020-07-24 16:17:30 -0300

    update

--- ansible/updatedev.sh
+++ ansible/updatedev.sh
@@ -6,6 +6,6 @@ ansible-playbook -i ./inventory/hosts update.yml \
             -e repository="git@ssh-repositorio-ce-asi.buenosaires.gob.ar:reingenieria-recibos-sueldo/rds-consumer.git" \
             -e folder="rds-consumer" \
             -e branch="dev" \
-            -e path="/java/rds-consumer" \
+            -e path="/prueba/rds-consumer" \
 
             -vv
