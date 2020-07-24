f7c49adc95bfb1c9e6e761f40418aa00903a12fb
Anibal Barbosa <abarbosa@buenosaires.gob.ar>
2020-07-24 14:07:22 -0300

    update ansible

--- ansible/installmaster.sh
+++ ansible/installmaster.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #   -e @extra-vars/default-parameters.json \
-ansible-playbook -i ./inventory/hosts install.yml \
+ansible-playbook -i ./inventory/hosts.dist install.yml \
             -u root \
             -e env=dev \
             -e repository="git@ssh-repositorio-ce-asi.buenosaires.gob.ar:reingenieria-recibos-sueldo/rds-consumer.git" \
             -e folder="rds-consumer" \
             -e branch="dev" \
-            -e path="/java/rds-consumer" \
+            -e path="/prueba/rds-consumer" \
             -e db_user="APPRECIBOS_DEMO" \
             -e db_pass="123456" \
             -e db_host="10.9.10.22" \
@@ -14,3 +14,4 @@ ansible-playbook -i ./inventory/hosts install.yml \
             -e db-port="1521"
             -vv
 
+-u root -e env=dev  -e repository="git@ssh-repositorio-ce-asi.buenosaires.gob.ar:reingenieria-recibos-sueldo/rds-consumer.git"  -e folder="rds-consumer"  -e branch="dev"  -e path="/prueba/rds-consumer"  -e db_user="APPRECIBOS_DEMO"  -e db_pass="123456" -e db_host="10.9.10.22" -e db-servicename="estab.gcba.gob.ar" -e db-port="1521"
