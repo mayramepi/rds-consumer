75b7aff4c6f47126f5aecbc2fcff1e0d69351ef1
Anibal Barbosa <abarbosa@buenosaires.gob.ar>
2020-07-24 14:16:45 -0300

    update ansible

--- ansible/installmaster.sh
+++ ansible/installmaster.sh
@@ -6,7 +6,7 @@ ansible-playbook -i ./inventory/hosts.dist install.yml \
             -e repository="git@ssh-repositorio-ce-asi.buenosaires.gob.ar:reingenieria-recibos-sueldo/rds-consumer.git" \
             -e folder="rds-consumer" \
             -e branch="dev" \
-            -e path="/prueba/rds-consumer" \
+            -e path="/prueba" \
             -e db_user="APPRECIBOS_DEMO" \
             -e db_pass="123456" \
             -e db_host="10.9.10.22" \
